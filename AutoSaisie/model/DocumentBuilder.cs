aba74c656ceb859f002df651bc954405954d66c1
zakaria <aboujenanez@gmail.com>
2022-08-13 20:51:51 +0100

    tests

---
+++ AutoSaisie/model/DocumentBuilder.cs
@@ -0,0 +1,30 @@
+﻿using AutoSaisie.DAO;
+using Objets100cLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSaisie.model
+{
+    class DocumentBuilder
+    {
+        public static IBODocumentStock3 buildDocumentStock(DateTime do_date,String do_ref,IBOCompteA3 CompteA,IBODepot3 depotStockage,)
+        {
+            IBSCIALApplication3 baseCial = new ConnectToDatabase().OuvrirBaseCIAL();
+            IBODocumentStock3 documentStock = baseCial.FactoryDocumentStock.CreateType(DocumentType.DocumentTypeStockMouvIn);
+            documentStock.DO_Date = do_date;
+            documentStock.DO_Ref = do_ref;
+            documentStock.CompteA = CompteA;
+            documentStock.DepotStockage = depotStockage;
+            return documentStock;
+        }
+        public static IBODocumentStockLigne3 buildDocumentStockLigne(IBODocumentStock3 documentStock, String ref_txt,int ref_int)
+        {
+            IBODocumentStockLigne3 documentLigne = (IBODocumentStockLigne3)documentStock.FactoryDocumentLigne.Create();
+            documentLigne.SetDefaultArticleReference(ref_txt,ref_int);
+            return documentLigne;
+        }
+    }
+}
