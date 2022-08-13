8fffbb61459c6500e48dc88027d7dde15850ed73
zakaria <aboujenanez@gmail.com>
2022-08-13 19:15:46 +0100

    tests

---
+++ AutoSaisie/DAO/ConnectToDatabase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Objets100cLib;
+
+namespace AutoSaisie.DAO
+{
+
+    class ConnectToDatabase
+    {
+        public IBSCPTAApplication3 baseComptable { get; set; }
+        public IBSCIALApplication3 baseCommerciale { get; set; }
+        public String errorMsg { get; set; }
+        public String seccessMsg { get; set; }
+
+        public ConnectToDatabase()
+        {
+            baseComptable = new BSCPTAApplication100c();
+            baseCommerciale = new BSCIALApplication100c();
+
+        }
+        public IBSCIALApplication3 OuvrirBaseCIAL()
+        {
+            try
+            {
+
+                baseCommerciale.Name = "C:/Users/Public/Documents/Sage/Entreprise 100c/Bijou.gcm";
+                baseCommerciale.Loggable.UserName = "<Administrateur>";
+                baseCommerciale.Loggable.UserPwd = "";
+                baseCommerciale.Open();
+                return baseCommerciale;
+
+
+            }
+            catch (Exception ex)
+            {
+                errorMsg = "erreur Base Commerciale : " + ex.Message;
+                throw ex;
+            }
+
+        }
+
+        public IBSCPTAApplication3 OuvrirBaseCOMP()
+        {
+            try
+            {
+
+                baseComptable.Name = "C:/Users/Public/Documents/Sage/Entreprise 100c/Bijou.mae";
+                baseComptable.Loggable.UserName = "<Administrateur>";
+                baseComptable.Loggable.UserPwd = "";
+                baseComptable.Open();
+                return baseComptable;
+
+            }
+            catch (Exception ex)
+            {
+                errorMsg = "erreur Base Comptable : " + ex.Message;
+                throw ex;
+            }
+
+
+        }
+
+    }
+}
---
+++ AutoSaisie/DAO/SaisieDocumentComptable.cs
@@ -0,0 +1,60 @@
+﻿using Objets100cLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSaisie.DAO
+{
+    class SaisieDocumentStock : ISaisie<IBODocumentStock3>
+    {
+        public ConnectToDatabase db { get; set; }
+
+        public IBSCIALApplication3 baseCIAL { get; set; }
+        public SaisieDocumentStock()
+        {
+            baseCIAL = db.OuvrirBaseCIAL();
+        }
+
+        public void ajouter(IBODocumentStock3 documentStock)
+        {
+            try
+            {
+                documentStock = baseCIAL.FactoryDocumentStock.CreateType(DocumentType.DocumentTypeStockMouvIn);
+                documentStock.DO_Date = DateTime.Now;
+                documentStock.DO_Ref = "ZAKI123";
+                documentStock.CompteA = getCodeAffaire(baseCIAL, "953INDE");
+                documentStock.DepotStockage = baseCIAL.FactoryDepot.ReadIntitule("Bijou SA");
+                documentStock.WriteDefault();
+                documentStock.CouldModified();
+
+                IBODocumentStockLigne3 documentLigne = (IBODocumentStockLigne3)documentStock.FactoryDocumentLigne.Create();
+                documentLigne.SetDefaultArticleReference("BAAR01", 5);
+                documentLigne.WriteDefault();
+
+                documentLigne = (IBODocumentStockLigne3)documentStock.FactoryDocumentLigne.Create();
+                documentLigne.SetDefaultArticleReference("CHFE01", 12);
+                documentLigne.WriteDefault();
+
+                db.seccessMsg = "document Added !";
+            }catch(Exception e)
+            {
+                db.errorMsg = "error creation document stock : " + e.Message;
+            }
+        }
+
+        public List<IBODocumentStock3> lister()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBOCompteA3 getCodeAffaire(IBSCIALApplication3 basecial, String codeAff)
+        {
+            IBPDossierParamCial dossierParamCial = (IBPDossierParamCial)basecial.FactoryDossierParam.List[1];
+            IBPAnalytique3 planAnalytique = dossierParamCial.AnalytiqueAffaire;
+            IBOCompteA3 compte = basecial.CptaApplication.FactoryCompteA.ReadNumero(planAnalytique, codeAff);
+            return compte;
+        }
+    }
+}
